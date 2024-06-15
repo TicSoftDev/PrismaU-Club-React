49814280a4d6094d20e53951e81aa92f87e1d0a1
Juan Felipe Arrieta Ortiz <ticsoft.desarrollador@gmail.com>
2024-06-15 11:46:38 -0500

    animacion a container de cards

--- src/components/dashboard/ContenedorDashboard.jsx
+++ src/components/dashboard/ContenedorDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ContenedorDashboard(props) {
     return (
-        <div className="flex flex-wrap my-6 font-semibold">
+        <div className="flex flex-wrap my-6 font-semibold animated-element">
             {props.children}
         </div>
     );
